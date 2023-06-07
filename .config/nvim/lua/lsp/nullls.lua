4602d16b79710ec619c761f4654abc11d449cebe
HoNamDuong <ducdat141198@gmail.com>
2023-06-07 17:00:43 +0700

    change: neofetch, nvim, picom

--- .config/nvim/lua/lsp/nullls.lua
+++ .config/nvim/lua/lsp/nullls.lua
@@ -8,7 +8,7 @@ local formatting = nullls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 -- local diagnostics = nullls.builtins.diagnostics
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 nullls.setup({
     debug = true,
@@ -33,16 +33,16 @@ nullls.setup({
             },
         }),
     },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
+    -- on_attach = function(client, bufnr)
+    --     if client.supports_method("textDocument/formatting") then
+    --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --         vim.api.nvim_create_autocmd("BufWritePre", {
+    --             group = augroup,
+    --             buffer = bufnr,
+    --             callback = function()
+    --                 vim.lsp.buf.format({ bufnr = bufnr })
+    --             end,
+    --         })
+    --     end
+    -- end,
 })
