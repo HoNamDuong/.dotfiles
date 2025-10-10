80e90bf4b5b5a2f8e7608a5144d224288b7d78f9
HoNamDuong <91533350+HoNamDuong@users.noreply.github.com>
2025-10-10 12:12:29 +0700

    fix(awesome): disable volume, mic, gpu, net in wibar

--- .config/awesome/ui/wibar/init.lua
+++ .config/awesome/ui/wibar/init.lua
@@ -11,12 +11,12 @@ local dashboard = require("ui.wibar.dashboard")
 local clock = require("ui.wibar.clock")
 local systray = require("ui.wibar.systray")
 local playerbox = require("ui.wibar.playerbox")
-local volume = require("ui.wibar.volume")
-local mic = require("ui.wibar.mic")
+-- local volume = require("ui.wibar.volume")
+-- local mic = require("ui.wibar.mic")
 local cpu = require("ui.wibar.cpu")
 local memory = require("ui.wibar.memory")
-local gpu = require("ui.wibar.gpu")
-local net = require("ui.wibar.net")
+-- local gpu = require("ui.wibar.gpu")
+-- local net = require("ui.wibar.net")
 
 screen.connect_signal("request::desktop_decoration", function(s)
     local is_primary = s == screen.primary
@@ -45,12 +45,12 @@ screen.connect_signal("request::desktop_decoration", function(s)
             },
             -- Right
             {
-                is_primary and net,
-                is_primary and gpu,
+                -- is_primary and net,
+                -- is_primary and gpu,
                 is_primary and memory,
                 is_primary and cpu,
-                is_primary and volume,
-                is_primary and mic,
+                -- is_primary and volume,
+                -- is_primary and mic,
                 is_primary and clock,
                 layoutlist(s),
                 minimization,
